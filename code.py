9f5e89151c2da1669e49727858f10e96929ee712
romainmsn <romain.masson@edhec.com>
2023-12-26 16:30:38 +0100

    Premier code

---
+++ code.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas_datareader as web
+import datetime
+from pandas_datareader import data as pdr
+import yfinance as yf
+yf.pdr_override()
+
+#import data
+
+
+
+#Bull spread
